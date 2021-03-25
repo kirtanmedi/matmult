86119fb2f8562c364b970a2a877dd2ea0d64c384
kirtanmedi <kirtanmedi@gmail.com>
2021-03-25 18:04:45 -0500

    file reading working with global vars

--- matmult.cpp
+++ matmult.cpp
@@ -2,16 +2,27 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <stdlib.h>
+#include <pthread.h>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char *argv[]){
+int u;
+int v;
+int w;
+vector<vector<int> > mat1;
+vector<vector<int> > mat2;
+vector<vector<int> > matmult;
+
+void *compute(void *args) {
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
     //getting command line arguments
-    int u;
     istringstream(argv[1]) >> u;
-    int v;
     istringstream(argv[2]) >> v;
-    int w;
     istringstream(argv[3]) >> w;
     string inputFile1 = argv[4];
     string inputFile2 = argv[5];
@@ -19,9 +30,11 @@ int main(int argc, char *argv[]){
     int threadCount;
     istringstream(argv[7]) >> threadCount;
 
-    //initializing the matrices
-    int matrix1[u][v];
-    int matrix2[v][w];
+    //checking if inputs are negative
+    if (u < 0 || v < 0 || w < 0 || threadCount < 1) {
+        cout << "Please enter a valid parameter.\n";
+        exit(0);
+    }
 
     //setting up file reading to read matrices into arrays
     ifstream file1(inputFile1.c_str());
@@ -29,11 +42,27 @@ int main(int argc, char *argv[]){
 
     int num;
 
+    //resizing matrices
+    mat1.resize(u);
+    for (int i = 0; i < u; i++) {
+        mat1[i].resize(v);
+    }
+
+    mat2.resize(v);
+    for (int i = 0; i < v; i++) {
+        mat2[i].resize(w);
+    }
+
+    matmult.resize(u);
+    for (int i = 0; i < u; i++) {
+        matmult.resize(w);
+    }
+
     //filling first matrix
     for (int i = 0; i < u; i++) {
         for (int j = 0; j < v; j++) {
             if (file1 >> num) {
-                matrix1[i][j] = num;
+                mat1[i][j] = num;
             }
         }
     }
@@ -43,9 +72,20 @@ int main(int argc, char *argv[]){
     for (int i = 0; i < v; i++) {
         for (int j = 0; j < w; j++) {
             if (file2 >> num) {
-                matrix2[i][j] = num;
+                mat2[i][j] = num;
             }
         }
     }
     file2.close();
+
+    //creating the array of threads
+    pthread_t t[threadCount];
+
+    for (int i = 0; i < threadCount; i++) {
+        pthread_create(&t[i], NULL, compute, NULL);
+    }
+
+    for (int i = 0; i < threadCount; i++) {
+        pthread_join(t[i], NULL);
+    }
 }
